466a6ccb68141ae0e8087ecb1f14573aa1376cd0
temujins <mdsifatulislam.rabbi@gmail.com>
2021-09-17 18:44:46 +0600

    updated views/CanvasViews.ts

--- src/views/CanvasView.ts
+++ src/views/CanvasView.ts
@@ -18,17 +18,36 @@ export class CanvasView {
         this.info = document.querySelector('#info');
     }
 
-    clear(): void {
+    public clear(): void {
         this.context?.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
-    initStartButton(startFunction: (view: CanvasView) => void): void {
+    public initStartButton(startFunction: (view: CanvasView) => void): void {
         this.start?.addEventListener('click', () => startFunction(this));
     }
 
-    drawScore(score: number): void {
-        if (this.scoreDisplay) {
-            this.scoreDisplay.innerHTML = score.toString();
-        }
+    public drawScore(score: number): void {
+        if (this.scoreDisplay) this.scoreDisplay.innerHTML = score.toString();
+    }
+
+    public drawInfo(text: string): void {
+        if (this.info) this.info.innerHTML = text;
+    }
+
+    private drawSprite(sprite: Brick | Paddle | Ball): void {
+        if (!sprite) return;
+
+        this.context?.drawImage(
+            sprite.image,
+            sprite.pos.x,
+            sprite.pos.y,
+            sprite.width,
+            sprite.height
+        );
+    }
+
+    public drawBricks(bricks: Brick[]): void {
+        if (!bricks) return;
+        bricks.forEach((brick) => this.drawSprite(brick));
     }
 }
